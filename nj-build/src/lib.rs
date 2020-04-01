920ad651b63601b481fbb82a4a99db8c7560e37f
Usagi Ito <the@usagi.network>
2020-04-02 06:22:58 +0900

    fix #5\nSupport non-macos UNIX like environments such as Linux

--- nj-build/src/lib.rs
+++ nj-build/src/lib.rs
@@ -2,5 +2,7 @@
 /// configure linker to generate node.js dynamic library
 pub fn configure() {
     println!("cargo:rustc-cdylib-link-arg=-undefined");
-    println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+    if cfg!(macos) {
+        println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+    }
 }
