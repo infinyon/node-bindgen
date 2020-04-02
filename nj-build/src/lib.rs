949897907ad4c31a133f7e3bf3aecc25e3bac21d
Tom Parker-Shemilt <palfrey@tevp.net>
2020-04-02 23:22:11 +0100

    Fix nj-build for macos

--- nj-build/src/lib.rs
+++ nj-build/src/lib.rs
@@ -2,7 +2,7 @@
 /// configure linker to generate node.js dynamic library
 pub fn configure() {
     println!("cargo:rustc-cdylib-link-arg=-undefined");
-    if cfg!(macos) {
+    if cfg!(target_os = "macos") {
         println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
     }
 }
