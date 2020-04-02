6d2609e8960ad84ea727159fdd6128e9c46726e4
Tom Parker-Shemilt <palfrey@tevp.net>
2020-04-02 23:06:01 +0100

    Limit options before nvm

--- travis.sh
+++ travis.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e -o pipefail
+
 source ~/.nvm/nvm.sh
-
-set -eux -o pipefail
-
-nvm install $NODE
+nvm install $NODE || exit 1
 nvm use $NODE
+
+# Can't set these until here, as nvm would fail
+set -ux
+
 cargo build
 cargo test
 cargo install --path nj-cli
