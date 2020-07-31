203cf3e63c74243eb5ca233d3c79c3bd31a5f16b
Ryan <ryan.michael.tate@gmail.com>
2020-07-31 12:31:41 -0700

    add delete_reference method to JsEnv; invoke after calling napi_wrap

--- nj-core/src/class.rs
+++ nj-core/src/class.rs
@@ -47,6 +47,15 @@ impl <T>JSObjectWrapper<T> where T: JSClass {
             rust_ref.wrapper = wrap;
         }
 
+        // Finally, remove the reference in response to finalize callback
+        // See footnote on `napi_wrap` documentation: https://nodejs.org/api/n-api.html#n_api_napi_wrap
+        // 
+        // "Caution: The optional returned reference (if obtained) should be deleted via napi_delete_reference 
+        // ONLY in response to the finalize callback invocation. If it is deleted before then, 
+        // then the finalize callback may never be invoked. Therefore, when obtaining a reference a 
+        // finalize callback is also required in order to enable correct disposal of the reference."
+        js_env.delete_reference(wrap);
+
         Ok(js_cb.this_owned())
     }
 }
@@ -153,7 +162,7 @@ pub trait JSClass: Sized {
         debug!("my object finalize");
         unsafe {
             let ptr: *mut JSObjectWrapper<Self> = finalize_data as *mut JSObjectWrapper<Self>;
-            let _rust = Box::from_raw(ptr);
+            Box::from_raw(ptr);
         }
         
     }
