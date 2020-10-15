52b412f27ebfe6505a82e2637e1252a55bcbaf25
Sehyo <sehyo@fluvio.io>
2020-10-15 03:34:35 +0000

    clippy and fmt

--- examples/cleanup/src/lib.rs
+++ examples/cleanup/src/lib.rs
@@ -6,14 +6,12 @@ use node_bindgen::core::NjError;
 
 /// initialize env hook up
 #[node_bindgen]
-fn init(env: JsEnv) -> Result<(),NjError> {
-
-    env.add_env_clean_up_hook(Some(my_cleanup),ptr::null_mut())?;
+fn init(env: JsEnv) -> Result<(), NjError> {
+    env.add_env_clean_up_hook(Some(my_cleanup), ptr::null_mut())?;
     println!("init");
     Ok(())
-
 }
 
-unsafe extern fn my_cleanup(_arg: *mut ::std::os::raw::c_void) {
+unsafe extern "C" fn my_cleanup(_arg: *mut ::std::os::raw::c_void) {
     println!("I'm called from node to do cleanup");
 }
