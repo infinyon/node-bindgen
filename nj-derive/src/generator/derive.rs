531acf5044b2bd96ed37c670ec1c68247deae8fb
Michał Kawalec <michal@monad.cat>
2021-04-14 12:34:40 +0200

    getting closer to generation

--- nj-derive/src/generator/derive.rs
+++ nj-derive/src/generator/derive.rs
@@ -3,12 +3,145 @@ use proc_macro2::TokenStream;
 use proc_macro2::Span;
 
 use syn::DeriveInput;
+use syn::Result;
+use syn::Data;
+use syn::DataStruct;
+use syn::Fields;
+use syn::FieldsNamed;
+use syn::FieldsUnnamed;
+use syn::Type;
+use syn::Ident;
+use syn::Error;
+use syn::spanned::Spanned;
+use syn::GenericParam;
+
+use crate::ast::MyTypePath;
+use crate::ast::MyReferenceType;
 
 pub fn generate_datatype(input_struct: DeriveInput) -> TokenStream {
-    println!("In gen datatype");
+    match Struct::from_ast(&input_struct) {
+        Err(err) => err.to_compile_error(),
+        Ok(parsed_struct) => {
+            println!("We got struct: {:?}", parsed_struct);
 
-    quote! {
-        // We are in a datatype gen
-        #input_struct
+            let try_into_js = generate_try_into_js(&parsed_struct);
+            quote! {
+                // We are in a datatype gen
+                #input_struct
+
+                #try_into_js
+            }
+        }
+    }
+}
+
+fn generate_try_into_js(parsed_struct: &Struct) -> TokenStream {
+    match parsed_struct {
+        Struct::Named { name, fields, generics } => {
+            quote! {
+                impl TryIntoJs for #name {
+                    fn try_to_js(self, js: &node_bindgen::core::val::JsEnv) ->
+                        Result<node_bindgen::core::sys::napi_value, 
+                               node_bindgen::core::NjError> 
+                    {
+                        unimplemented!()
+                    }
+                }
+            }
+        },
+        _ => unimplemented!()
+    }
+}
+
+#[derive(Debug)]
+pub enum Struct<'a> {
+    Named {
+        name: &'a Ident,
+        fields: Vec<Field<'a>>,
+        generics: Vec<&'a GenericParam>
+    },
+    Unnamed {
+        name: &'a Ident,
+        fields: Vec<FieldType<'a>>,
+        generics: Vec<&'a GenericParam>
+    },
+    Unit {
+        name: &'a Ident
+    }
+}
+
+#[derive(Debug)]
+pub struct Field<'a> {
+    name: &'a Ident,
+    ty: FieldType<'a>
+}
+
+#[derive(Debug)]
+pub enum FieldType<'a> {
+    Path(MyTypePath<'a>),
+    Ref(MyReferenceType<'a>),
+}
+
+impl<'a> FieldType<'a> {
+    pub fn from(ty: &'a Type) -> Result<Self> {
+        match ty {
+            Type::Path(type_path) => Ok(FieldType::Path(
+                MyTypePath::from(type_path)?)),
+            Type::Reference(reference) => Ok(FieldType::Ref(
+                MyReferenceType::from(reference)?)),
+            _ => Err(Error::new(ty.span(), "Only type paths and references \
+                    are supported as field types")),
+        }
+    }
+}
+
+impl<'a> Struct<'a> {
+    pub fn from_ast(input: &'a DeriveInput) -> Result<Struct> {
+        let struct_data = match &input.data {
+            Data::Struct(inner_struct) => Ok(inner_struct),
+            Data::Enum(_) => Err(Error::new(input.span(), "Enums are not supported \
+                for automatic conversion to JavaScript representation")),
+            Data::Union(_) => Err(Error::new(input.span(), "Unions are not supported \
+                for automatic conversion to JavaScript representation")),
+        }?;
+
+        let generics = input.generics.params
+            .iter()
+            .collect();
+
+        match &struct_data.fields {
+            Fields::Named(named_fields) => {
+                let fields = named_fields.named
+                    .iter()
+                    .filter_map(|field| field.ident.as_ref().map(|ident| (ident, &field.ty)))
+                    .map(|(ident, ty)| {
+                        FieldType::from(&ty)
+                            .map(|ty| Field {
+                                name: &ident,
+                                ty
+                            })
+                    })
+                    .collect::<Result<Vec<Field<'a>>>>()?;
+
+                Ok(Struct::Named {
+                    name: &input.ident,
+                    fields,
+                    generics,
+                })
+            },
+            Fields::Unnamed(unnamed_fields) => {
+                let fields = unnamed_fields.unnamed
+                    .iter()
+                    .map(|field| FieldType::from(&field.ty))
+                    .collect::<Result<Vec<FieldType<'a>>>>()?;
+
+                Ok(Struct::Unnamed {
+                    name: &input.ident,
+                    fields,
+                    generics,
+                })
+            },
+            Fields::Unit => Ok(Struct::Unit { name: &input.ident })
+        }
     }
 }
--- nj-derive/ui-tests/pass_struct.rs
+++ nj-derive/ui-tests/pass_struct.rs
@@ -1,4 +1,6 @@
 use node_bindgen::derive::node_bindgen;
+use node_bindgen::core::TryIntoJs;
+use std::fmt::Debug;
 
 #[node_bindgen]
 struct Something {
@@ -6,8 +8,30 @@ struct Something {
 }
 
 #[node_bindgen]
-pub(crate) struct SomethingElse {
+pub(crate) struct WithVisibility {
     pub field: usize
+}
+
+#[node_bindgen]
+struct Generic<T> {
+    pub field: T
+}
+
+#[node_bindgen]
+struct Lifetime<'a> {
+    pub field: &'a usize
+}
+
+#[node_bindgen]
+struct BoundGeneric<T>
+    where T: Sync + Debug
+{
+    pub field: T
+}
+
+#[node_bindgen]
+struct BoundAndLifetimes<'a, T: Sync + Debug> {
+    pub field: &'a T
 }
 
 fn main() {
