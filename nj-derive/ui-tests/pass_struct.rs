99e4ad06502490d198b5e1eca440d1afbba99d09
Michał Kawalec <michal@monad.cat>
2021-04-19 12:31:33 +0200

    looks like we generate successfully

--- nj-derive/ui-tests/pass_struct.rs
+++ nj-derive/ui-tests/pass_struct.rs
@@ -11,25 +11,27 @@ pub(crate) struct WithVisibility {
 }
 
 #[node_bindgen]
-struct Generic<T> {
-    pub field: T
-}
-
-#[node_bindgen]
 struct Lifetime<'a> {
     pub field: &'a usize
 }
 
 #[node_bindgen]
 struct BoundGeneric<T>
-    where T: Sync + std::fmt::Debug
+    where T: Sync + std::fmt::Debug + node_bindgen::core::TryIntoJs + Clone
 {
     pub field: T
 }
 
 #[node_bindgen]
-struct BoundAndLifetimes<'a, T: Sync + std::fmt::Debug> {
+struct BoundAndLifetimes<'a, T: Sync + std::fmt::Debug + node_bindgen::core::TryIntoJs + Clone> {
     pub field: &'a T
+}
+
+#[node_bindgen]
+struct Simple {
+    pub a_string: String,
+    pub a_number: i64,
+    pub a_float : f64
 }
 
 fn main() {
