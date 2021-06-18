33adc1a65839c7c19e81267ccaa7687e83436a11
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-06-18 11:28:33 -0700

    Allowed deref_nullptr for tests in nj-sys

--- nj-sys/src/lib.rs
+++ nj-sys/src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+//https://github.com/rust-lang/rust-bindgen/issues/1651
+#![cfg_attr(test, allow(deref_nullptr))]
 mod binding;
 
 pub use binding::*;
