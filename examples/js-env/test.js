4bf72be52e839be6ad87565dc1167cfccaf10320
Sehyo Chang <sehyo@fluvio.io>
2023-09-24 22:00:36 -0700

    revert jsenv test

--- examples/js-env/test.js
+++ examples/js-env/test.js
@@ -2,5 +2,4 @@ const assert = require('assert');
 
 let addon = require('./dist');
 
-console.print(addon.multiply(5.0));
-assert.equal(addon.multiply(5), 10.0);
+assert.equal(addon.multiply(5), 10);
