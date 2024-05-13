86902cf36495ea9abc2a754007104d65c14efded
Thomas Frank <thomas.frank@esrlabs.com>
2024-05-13 11:36:58 +0200

    fix more clippy warnings

--- examples/class-async/src/lib.rs
+++ examples/class-async/src/lib.rs
@@ -2,28 +2,7 @@ use std::time::Duration;
 
 use fluvio_future::timer::sleep;
 
-use node_bindgen::sys::napi_value;
-use node_bindgen::core::NjError;
-use node_bindgen::core::val::JsObject;
-use node_bindgen::core::val::JsEnv;
-use node_bindgen::core::TryIntoJs;
 use node_bindgen::derive::node_bindgen;
-
-struct MyJson {
-    val: f64,
-}
-
-impl TryIntoJs for MyJson {
-    fn try_to_js(self, js_env: &JsEnv) -> Result<napi_value, NjError> {
-        // create JSON
-        let mut json = JsObject::new(*js_env, js_env.create_object()?);
-
-        let js_val = js_env.create_double(self.val)?;
-        json.set_property("val", js_val)?;
-
-        json.try_to_js(js_env)
-    }
-}
 
 struct MyObject {
     val: f64,
